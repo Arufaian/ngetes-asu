04d9299f8fd195ea46828f8b6bc2735b80bd68c7
Alfian <alfianilyasya13@gmail.com>
2024-05-01 17:06:41 +0700

    wanjay

--- javascript/test.js
+++ javascript/test.js
@@ -3,5 +3,12 @@ let audio = new Audio("../resource/klek.mp3");
 let trigerButton = document.querySelector(".triggerButton");
 
 trigerButton.addEventListener("click", function () {
-  audio.play();
+  //   console.log(this.id);
+  let name = this.id;
+  playAudio(name);
 });
+
+function playAudio(name) {
+  let audio = new Audio(`resource/${name}.mp3`);
+  audio.play();
+}
